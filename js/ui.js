c610216d50e0d8868fb9b2acd7d863ea28ef8eb7
hiszpan93 <hiszpan_93@tlen.pl>
2025-06-06 20:09:39 +0200

    wweeeez

--- js/ui.js
+++ js/ui.js
@@ -674,7 +674,11 @@ document.getElementById("calc-btn").addEventListener("click", () => calculatePay
 
   
   
-  
+  document.getElementById("resetTournamentBtn").addEventListener("click", () => {
+    if (typeof resetTournamentData === "function") {
+      resetTournamentData();
+    }
+  });
   
   // ======= FADE-IN ELEMENTÓW INTERFEJSU =======
   function fadeInElement(el) {
